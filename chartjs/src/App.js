e720a707e2ba83f869c62203b7659f81a1a59046
kyun <zozi95@naver.com>
2021-04-23 10:32:15 +0900

    CHART JS

---
+++ chartjs/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BarChart from "./components/BarChart";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <BarChart />
+    </div>
+  );
+};
+
+export default App;
